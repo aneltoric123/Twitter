01c9c48f454b3b02c0f4b57410208b1edbcaed95
aneltoric123 <toricanel928@gmail.com>
2024-02-12 18:26:07 +0100

    Following test

--- db/migrate/20240209164212_create_followings.rb
+++ db/migrate/20240209164212_create_followings.rb
@@ -1,8 +1,8 @@
 class CreateFollowings < ActiveRecord::Migration[7.1]
   def change
     create_table :followings do |t|
-      t.integer :follower_id,foreign_key: { to_table: :users }
-      t.integer :followed_id,foreign_key: { to_table: :users }
+      t.references :follower, foreign_key: { to_table: :users }
+      t.references :followed, foreign_key: { to_table: :users }
 
       t.timestamps
     end
